7e0264c97d0685ecdd9ffdc35c44cd0ef5522976
Sanjanaaa18 <sanjanasanjuu18@gmail.com>
2025-08-08 16:37:50 +0530

    employee

---
+++ Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    internal class Person
+    {
+        //auto-implemented properties for Person class
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+        
+
+        public Person() // Default constructor
+        {
+        }
+        public Person(int id, string name,  string city, int age) // Parameterized constructor
+        {
+            Id = id;
+            Name = name;
+            City = city;
+            Age = age;
+        }
+        // Method to display person details
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"City: {City}");
+            Console.WriteLine($"Age: {Age}");
+        }
+        public string GetDetails()
+        {
+            return $"Id: {Id}, Name: {Name}, City: {City}, Age: {Age}";
+        }
+        // Method to update person details
+        public void UpdateDetails(int id, string name, string city, int age)
+        {
+            Id = id;
+            Name = name;
+            City = city;
+            Age = age;
+        }
+
+    }
+}
